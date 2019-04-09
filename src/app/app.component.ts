650bb80f36c84373a1a4a7ff84320f520e678fa4
yugijudai3 <yugiohocg123@gmail.com>
2019-04-10 00:34:59 +0900

    55

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
   title = 'testapp';
+  calendarPlugins = [dayGridPlugin, timeGridPlugin, interactionPlugin];
+  calendarWeekends = true;
+  calendarVisible = true;
+  selectable = true;
+  calendarEvents: EventInput[] = [
+    {title:"Event Now", start:new Date()}
+  ];
+
+  toggleVisible(){
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends(){
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+  }
 }
